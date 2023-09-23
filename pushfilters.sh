e5196ffb6701179a34b1fed0a7d21a44bcc01210
Lee Hetherington <70320109+edgenative@users.noreply.github.com>
2023-09-23 08:10:40 -0700

    Update description of the run for $1 $2 and $3

--- pushfilters.sh
+++ pushfilters.sh
@@ -13,7 +13,7 @@ fi
 # Read the input file line by line
 while IFS=',' read -r param1 param2 param3; do
     if [ -n "$param1" ] && [ -n "$param2" ]; then
-        # Run bgpq4 to fetch the prefixes, with ASN $param1 and AS-SET $param2 as arguments
+        # Run the python, with ASN $param1, slug $param2 and the router IP Address as $param3. Run for both IPv4 and IPv6
 	python3 $path/bin/mikrotik_irr_updater.py as$param1-$param2-import-ipv4 $path/filters/as$param1-$param2-import-ipv4.txt $param3
 	python3 $path/bin/mikrotik_irr_updater.py as$param1-$param2-import-ipv6 $path/filters/as$param1-$param2-import-ipv6.txt $param3
     fi
