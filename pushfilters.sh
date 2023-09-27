b5b7b201ee4180d3d67b25d4b20128fb116014f3
Lee Hetherington <70320109+edgenative@users.noreply.github.com>
2023-09-26 18:46:21 -0700

    Update pushfilters.sh

--- pushfilters.sh
+++ pushfilters.sh
@@ -14,7 +14,7 @@ fi
 while IFS=',' read -r param1 param2 param3; do
     if [ -n "$param1" ] && [ -n "$param2" ]; then
         # Run the python, with ASN $param1, slug $param2 and the router IP Address as $param3. Run for both IPv4 and IPv6
-	python3 $path/bin/mikrotik_irr_updater.py as$param1-$param2-import-ipv4 $path/filters/as$param1-$param2-import-ipv4.txt $param3
-	python3 $path/bin/mikrotik_irr_updater.py as$param1-$param2-import-ipv6 $path/filters/as$param1-$param2-import-ipv6.txt $param3
+	python3 $path/bin/mikrotik-irrupdater.py as$param1-$param2-import-ipv4 $path/filters/as$param1-$param2-import-ipv4.txt $param3
+	python3 $path/bin/mikrotik-irrupdater.py as$param1-$param2-import-ipv6 $path/filters/as$param1-$param2-import-ipv6.txt $param3
     fi
 done < $path/config/sessions.conf
